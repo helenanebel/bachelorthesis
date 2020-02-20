948824706b1ecfd4bd5f4967f2076317d922796e
Helena_Nebel <Helena.Nebel@dainst.de>
2020-02-20 18:43:58 +0100

    Script for reading MARC-files

---
+++ BA/read_marc.py
@@ -0,0 +1,12 @@
+from pymarc import MARCReader
+
+filestring = 'dai01_kexpm101'
+count = 0
+# out = open('records/hsozkult/hsozkult_to_recent/hsozkult_to_recent' + '_' + str(count) + '.mrc', 'wb')
+print(filestring + '.mrc')
+with open(filestring, 'rb') as file:
+    new_reader = MARCReader(file)
+    print(new_reader)
+    print()
+    for record in new_reader:
+        print(record)
