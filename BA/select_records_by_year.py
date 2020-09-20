17f69d871bd0a251910b3bc583fcf43d1a92d442
Helena_Nebel <Helena.Nebel@dainst.de>
2020-09-20 16:08:43 +0200

    Identify records with publication date in chosen range or missing date

--- BA/select_records_by_year.py
+++ BA/select_records_by_year.py
@@ -1,29 +1,30 @@
 from pymarc import MARCReader
 import write_error_to_logfile
 import re
+import os
 
-
-with open('selected_records.mrc', 'wb') as selected_record_file:
-    for filestring in ['dai01_kexpm101', 'dai01_kexpm102', 'dai01_kexpm103', 'dai01_kexpm104', 'dai01_kexpm105',
-                       'dai01_kexpm106', 'dai01_kexpm107', 'dai01_kexpm108', 'dai01_kexpm109', 'dai01_kexpm110',
-                       'dai01_kexpm111']:
-        count = 0
-        print(filestring)
-        with open(filestring, 'rb') as file:
+count = 0
+with open('selected_records_1960.mrc', 'wb') as selected_record_file:
+    for filestring in os.listdir('C:/Users/Helena_Nebel/Desktop/Zenon_Daten'):
+        with open('C:/Users/Helena_Nebel/Desktop/Zenon_Daten/' + filestring, 'rb') as file:
             try:
                 reader = MARCReader(file, force_utf8=True)
                 for record in reader:
                     try:
-                        if [field['c'] for field in record.get_fields('260', '264')]:
-                            dates = [re.findall(r'\d{4}', field['c']) for field in record.get_fields('260', '264') if field['c']]
-                            for dates_list in dates:
-                                if any([(1979 < int(year) < 1990) for year in dates_list]):
-                                    selected_record_file.write(record.as_marc21())
-                                    count += 1
-                                if not dates_list:
-                                    print(record['001'], [field['c'] for field in record.get_fields('260', '264')])
-                                    selected_record_file.write(record.as_marc21())
-                                    count += 1
+                        if record.get_fields('260', '264'):
+                            if [field['c'] for field in record.get_fields('260', '264')]:
+                                dates = [re.findall(r'\d{4}', field['c']) for field in record.get_fields('260', '264')
+                                         if field['c']]
+                                for dates_list in dates:
+                                    if any([(1959 < int(year) < 1970) for year in dates_list]):
+                                        selected_record_file.write(record.as_marc21())
+                                        count += 1
+                                    if not dates_list:
+                                        print(record['001'], [field['c'] for field in record.get_fields('260', '264')])
+                                        selected_record_file.write(record.as_marc21())
+                                        count += 1
+                            else:
+                                selected_record_file.write(record.as_marc21())
                     except Exception as e:
                         print('Error', record['001'])
                         write_error_to_logfile.write(e)
