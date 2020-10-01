9127e919ad51b62d3b3a59aa5516ac1ed57e9567
Helena_Nebel <Helena.Nebel@dainst.de>
2020-10-01 22:02:21 +0200

    Update path to .mrc-files

--- BA/write_error_to_logfile.py
+++ BA/write_error_to_logfile.py
@@ -3,7 +3,7 @@ import os
 import logging
 from datetime import datetime
 
-logging.getLogger('chardet.charsetprober').setLevel(logging.INFO)
+logging.getLogger('chardet.charsetprober').setLevel(logging.DEBUG)
 
 dateTimeObj = datetime.now()
 timestampStr = dateTimeObj.strftime("%d-%b-%Y")
@@ -17,6 +17,7 @@ def write(e):
     exc_type, exc_obj, exc_tb = sys.exc_info()
     filename = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     error_message = 'Error! Code: {c}, Message, {m}, Type, {t}, File, {f}, Line {line}'.format(c=type(e).__name__, m=str(e), t=exc_type, f=filename, line=exc_tb.tb_lineno)
+    print(error_message)
     logger.debug(error_message)
 
 
